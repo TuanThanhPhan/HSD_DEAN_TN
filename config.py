1b0ab67ffa2f59a9e17503661dbae3aca94d5708
Thanh Phan <thanhphan@users.noreply.github.com>
2026-03-16 15:36:07 +0700

    fix dataset path in config

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ PATIENCE = 5
 MODEL_NAME = "vinai/phobert-base"
 MODEL_TYPE = "hybrid"   # phobert | visobert | hybrid
 
-TRAIN_PATH = "data/processed/train.csv"
-DEV_PATH = "data/processed/dev.csv"
-TEST_PATH = "data/processed/test.csv"
+TRAIN_PATH = "data/processed/final_train.csv"
+DEV_PATH = "data/processed/final_dev.csv"
+TEST_PATH = "data/processed/final_test.csv"
 
 SAVE_DIR = "checkpoints"
 
